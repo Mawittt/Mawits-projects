42baaa14ac06232f908f5e05a0c8bff45fbcf780
mawit <jackheaven023@gmail.com>
2022-07-16 13:23:30 +0100

    added / end point

--- src/index.js
+++ src/index.js
@@ -9,5 +9,10 @@ app.get("/project1", (req, res) => {
     res.sendFile(path.join(__dirname+"/public/index.html")) 
 })
 
+app.get("/",(req,res)=>{
+    res.send("Hello World")
+}
+)
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
