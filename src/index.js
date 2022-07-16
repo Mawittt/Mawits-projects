7df03237725bfcb5d77609d18cd48a3510f94dc5
mawit <jackheaven023@gmail.com>
2022-07-16 13:15:41 +0100

    no serious change

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 5000
+const path = require("path")
 
-app.use
+app.use(express.static(path.join(__dirname,"public")))
 
-app.get("/page", (req, res) => {
-    res.send("becky is a developer now") 
+app.get("/project1", (req, res) => {
+    res.sendFile(path.join(__dirname+"/public/index.html")) 
 })
 
 
